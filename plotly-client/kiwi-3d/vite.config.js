f8b9cefb13cb6f4551c8715e830f353d033bf580
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2026-04-14 10:11:06 -0400

    Fixed vite build

--- plotly-client/kiwi-3d/vite.config.js
+++ plotly-client/kiwi-3d/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
   base: "/3d/",
   build: {
-    outDir: path.resolve(__dirname, "../plotly-client/web/3d"),
+    outDir: "dist",
     emptyOutDir: true
   }
 });
